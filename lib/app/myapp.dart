705d2a84ffef0d259488f7bbf9c4ffe91e948e46
Demetrius Massard <demetrius2597@hotmail.com>
2025-05-07 15:33:20 -0300

    initial commit

---
+++ lib/app/myapp.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas_2/app/homepage/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista tarefas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Homepage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas_2/app/myapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
